a7e06ab334e8562fa8b1a6cfad3460d59a9ca30f
SpringHack <springhack@live.cn>
2016-04-25 21:53:25 +0800

    Add encodeSource

--- javascript/AJC.js
+++ javascript/AJC.js
@@ -1,6 +1,11 @@
 /**
         Author: SpringHack - springhack@live.cn
-        Last modified: 2016-04-20 13:29:29
+        Last modified: 2016-04-25 21:53:05
         Filename: AJC.js
         Description: Created by SpringHack using vim automatically.
 **/
+
+function encodeSource()
+{
+	return true;
+}
