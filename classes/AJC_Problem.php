2319593800bad29710a7375d7c3c0421f7e34708
SpringHack <springhack@live.cn>
2016-04-27 12:37:02 +0800

    P

--- classes/AJC_Problem.php
+++ classes/AJC_Problem.php
@@ -1,6 +1,26 @@
 <?php /**
         Author: SpringHack - springhack@live.cn
-        Last modified: 2016-04-20 13:30:44
-        Filename: ../../alxwvj_judge_core/classes/AJC_Problem.php
+        Last modified: 2016-04-27 12:36:47
+        Filename: AJC_Problem.php
         Description: Created by SpringHack using vim automatically.
 **/ ?>
+<?php
+
+	require_once(dirname(__FILE__)."/DataPoster.php");
+
+	class AJC_Problem {
+
+		public function AJC_Problem($id = 1000)
+		{
+		}
+
+		public function getInfo()
+		{
+		}
+
+		public function submitCode($oj = "AJC", $id = "1000", $lang = "0", $code = "", $cid = '0')
+		{
+		}
+
+	}
+?>
