4cfb19b64403a4ff71e1c3b859fca0011077cdce
SpringHack <springhack@live.cn>
2016-05-11 14:01:57 +0800

    Add base64image plugin in ckeditor.

--- vj_root/ckeditor/config.js
+++ vj_root/ckeditor/config.js
@@ -1,10 +1,12 @@
 /**
  * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
- */
+        Last modified: 2016-05-11 14:00:35
+        Filename: config.js
+**/
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.extraPlugins = "base64image";
 };
